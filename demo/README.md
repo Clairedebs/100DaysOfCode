98604833aaa8d5ac24f8a95d2ea67d949aff3e36
Clairedebs <clairedeborah890@gmail.com>
2025-09-12 21:48:54 +0100

    Add initial Angular application setup with configuration files, routing, and basic components

---
+++ demo/README.md
@@ -0,0 +1,7 @@
+Create a sample spring boot with the maven configuration and the stater Spring MVC
+
+#### Create Controller
+- We create a package named controllers at the path src/main/com/java/com/example/demo 
+- Create a java class that we named SimpleControllers and decorate with the `@RestController` annotation
+- Create endpoints of our API
+- Use @`GetMapping` for GET request
--- demo/src/main/java/com/example/demo/controllers/SimpleControllers.java
+++ demo/src/main/java/com/example/demo/controllers/SimpleControllers.java
@@ -1,4 +1,22 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SimpleControllers {
+
+    @GetMapping
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Request done successfully !");
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<String> greeting( @PathVariable String name ) {
+        return ResponseEntity.ok("Request send by : " + name);
+    }
+
 }
